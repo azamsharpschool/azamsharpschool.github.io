a77cd3d9600f64ebfdd795ad24f172631e278ce8
Mohammad Azam <azamsharp@gmail.com>
2024-09-17 08:17:01 -0500

    Added SwiftData workshop on the home page

--- js/recentSales.js
+++ js/recentSales.js
@@ -1,12 +1,12 @@
 // This is the history of actual sales from AzamSharp School. 
 // This is not live data and the sales happened in the past. 
 const recentSales = [
-    "Ociel purchased Supase for iOS Developers on August 29", 
-    "Shakir purchased Pro Membership on September 3", 
-    "Lucas purchased Pro Membership on September 3", 
-    "Peter purchased Pro Membership on September 6",
-    "Jean purchased Pro Membership September 9", 
-    "Ned purchased Supabase for iOS Developers Workshop on September 4"
+    "Ociel purchased Supase for iOS Developers.", 
+    "Shakir purchased Pro Membership.", 
+    "Lucas purchased Pro Membership.", 
+    "Peter purchased Pro Membership.",
+    "Jean purchased Pro Membership.", 
+    "Ned purchased Supabase for iOS Developers Workshop."
 ]
 
 function getRecentSales() {
